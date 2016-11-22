46e17c63ccca06a8c25295a68f2f80d516cfe3f4
James Fairbairn <jaybono30@hotmail.com>
2016-11-22 07:37:42 +0000

    Updated Docker Configuration

--- docker-image-config-3/scripts/entrypoint.sh
+++ docker-image-config-3/scripts/entrypoint.sh
@@ -4,9 +4,9 @@ echo "=> add-user-process: creating jboss users"
 ${JBOSS_HOME}/bin/add-user.sh --silent -e -u ${WILDFLY_MANAGEMENT_USER} -p ${WILDFLY_MANAGEMENT_PASSWORD}
 echo "=> add-user-process: jboss users have been created"
 
-sed -i "s/@DB_URL@/${DB_URL}/" ${JBOSS_HOME}/bin/commands.cli
-sed -i "s/@DB_USER@/${DB_USER}/" ${JBOSS_HOME}/bin/commands.cli
-sed -i "s/@DB_PASSWORD@/${DB_PASSWORD}/" ${JBOSS_HOME}/bin/commands.cli
+sed -i "s,@DB_URL@,${DB_URL}," ${JBOSS_HOME}/bin/commands.cli
+sed -i "s,@DB_USER@,${DB_USER}," ${JBOSS_HOME}/bin/commands.cli
+sed -i "s,@DB_PASSWORD@,${DB_PASSWORD}," ${JBOSS_HOME}/bin/commands.cli
 
 if [ "${NETWORK_MODE}" = "host" ]
 then
@@ -20,6 +20,10 @@ echo "=> configuration-process: using WildFly Config : ${JBOSS_CONFIG}"
 echo "=> configuration-process: starting WildFly server"
 ${JBOSS_HOME}/bin/standalone.sh -c ${JBOSS_CONFIG} -Djboss.bind.address=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.node.name=$HOSTNAME-$IPADDR > /dev/null &
 echo "=> configuration-process: waiting for WildFly to start on $IPADDR"
+	
+echo "=> configuration-process: sleeping for ${WAIT_TIME_SECS} seconds"
+sleep ${WAIT_TIME_SECS}
+echo "=> configuration-process: waking up"		
 	
 echo "=> configuration-process: checking that WildFly has started"
 ${JBOSS_HOME}/wait-for-it.sh $IPADDR:9990 -- echo "=> configuration-process: WildFly has started"
